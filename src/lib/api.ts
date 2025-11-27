c285e8132c225ca59c6b528f846ff9be01fd9a16
antonio <antonio.popa.dev@gmail.com>
2025-11-27 12:29:14 +0200

    Initial commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://buget-back-end.onrender.com/api';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
